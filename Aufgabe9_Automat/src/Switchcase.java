542521ba8b35e8eba03dd270ebd2d868d549776f
murmal <33840470+murmal@users.noreply.github.com>
2018-01-08 16:14:25 +0100

    Update Switchcase.java

--- Aufgabe9_Automat/src/Switchcase.java
+++ Aufgabe9_Automat/src/Switchcase.java
@@ -1,8 +1,74 @@
-
 public class Switchcase {
 
-	enum States {
-	
-	
-	
+	public enum State {
+		Z0, Z1, Z2, Z3, Z4;
+
+	}
+
+	State state = State.Z0;
+
+	public void setState(State state) {
+		this.state = state;
+	}
+
+	public void trigger(int trigger) {
+		boolean zero = (trigger == 0);
+		boolean one = (trigger == 1);
+		if (trigger <= 1 && trigger >= 0) {
+
+			switch (state) {
+
+			case Z0:
+				if (zero || one) {
+					setState(State.Z1);
+				}
+				break;
+
+			case Z1:
+				if (zero) {
+					setState(State.Z2);
+				} else if (one) {
+					setState(State.Z3);
+				}
+				break;
+
+			case Z2:
+				if (zero) {
+					setState(State.Z2);
+				} else if (one) {
+					setState(State.Z3);
+				}
+				break;
+
+			case Z3:
+				if (zero) {
+					setState(State.Z2);
+				} else if (one) {
+					setState(State.Z3);
+				}
+				break;
+
+			case Z4:
+				if (zero) {
+					setState(State.Z2);
+				} else if (one) {
+					setState(State.Z3);
+				}
+				break;
+
+			}
+			System.out.println("You're in state " + state + " now. Your trigger was '" + trigger + "'.");
+		} else {
+			System.out.println("Wrong trigger.");
+		}
+
+	}
+
+	public void clear() {
+
+		state = State.Z0;
+
+		System.out.println("The machine has been cleared.");
+	}
+
 }
