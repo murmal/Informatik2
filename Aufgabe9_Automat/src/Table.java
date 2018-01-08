02da869d17062d821f41c52322335bfa05a4c43e
murmal <33840470+murmal@users.noreply.github.com>
2018-01-08 16:14:42 +0100

    Update Table.java

--- Aufgabe9_Automat/src/Table.java
+++ Aufgabe9_Automat/src/Table.java
@@ -1,18 +1,36 @@
 
 public class Table {
-	
-	
-	public static void main(String[]args) {
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
+	int[][] table = { { 1, 1 }, // Z0
+			{ 2, 3 }, // Z1
+			{ 2, 4 }, // Z2
+			{ 2, 3 }, // Z3
+			{ 2, 3 } // Z4
+	};
+
+	int currentState = 0;
+
+	public void trigger(int trigger) {
+		int state = 0;
+
+		if (trigger <= 1 && trigger >= 0) {
+
+			state = table[currentState][trigger];
+
+			currentState = state;
+			System.out.println("You're in state Z" + state + " now. Your trigger was '" + trigger + "'.");
+
+		} else {
+			System.out.println("Wrong trigger.");
+		}
+
 	}
-	
-	
+
+	public void clear() {
+
+		currentState = 0;
+
+		System.out.println("The machine has been cleared.");
+	}
+
 }
